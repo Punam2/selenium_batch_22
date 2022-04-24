31faefbf45bd2f5884c77cd1d6d84d1c9738fbe8
Punam2 <104296657+Punam2@users.noreply.github.com>
2022-04-24 21:12:25 +0530

    Update HandleFrame.java

--- com.selenium/src/com/handlealertpopup/HandleFrame.java
+++ com.selenium/src/com/handlealertpopup/HandleFrame.java
@@ -5,7 +5,7 @@ public class HandleFrame {
 	public static void main(String[] args) {
 
 		System.out.println("Hi let's handle frame in selenium");
-		
+		System.out.println("iframe handle already");
 	}
 
 }
